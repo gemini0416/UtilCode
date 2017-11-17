a9ebbb843b8c21cdb1ae9c9d7dad6053ed478bd2
huangli <huangli@eptonic.com>
2017-11-17 11:43:41 +0800

    v1.0.0

---
+++ utils/src/main/java/com/utilcode/utils/IOUtils.java
@@ -0,0 +1,51 @@
+package com.utilcode.utils;
+
+import java.io.Closeable;
+import java.io.IOException;
+
+/**
+ * <pre>
+ *     desc  : io相关工具类
+ * </pre>
+ */
+public class IOUtils {
+
+    private IOUtils() {
+        throw new UnsupportedOperationException("u can't instantiate me...");
+    }
+
+    /**
+     * 关闭IO
+     *
+     * @param closeables closeable
+     */
+    public static void closeIO(Closeable... closeables) {
+        if (closeables == null) return;
+        for (Closeable closeable : closeables) {
+            if (closeable != null) {
+                try {
+                    closeable.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    /**
+     * 安静关闭IO
+     *
+     * @param closeables closeable
+     */
+    public static void closeIOQuietly(Closeable... closeables) {
+        if (closeables == null) return;
+        for (Closeable closeable : closeables) {
+            if (closeable != null) {
+                try {
+                    closeable.close();
+                } catch (IOException ignored) {
+                }
+            }
+        }
+    }
+}
